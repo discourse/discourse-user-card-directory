e6355395831128f98c425c35370b40121a503758
Penar Musaraj <pmusaraj@gmail.com>
2024-10-07 15:05:36 -0400

    DEV: update core extension of UserCardContents (#37)
    Should fix tests, they were failing with this error:
    
    Attempted to register a view with an id already in use: user-card...
    
    Co-authored-by: Sérgio Saquetim <1108771+megothss@users.noreply.github.com>

--- javascripts/discourse/components/user-card-static.js
+++ javascripts/discourse/components/user-card-static.js
@@ -1,20 +1,20 @@
 import { action } from "@ember/object";
 import UserCardContents from "discourse/components/user-card-contents";
 
-export default UserCardContents.extend({
-  layoutName: "components/user-card-contents",
-  elementId: null,
+export default class UserCardStaticContents extends UserCardContents {
+  layoutName = "components/user-card-contents";
+  elementId = null;
   // Overriding functions which cause the user card to show/hide based on mouse/keyboard events:
-  cleanUp() {},
+  cleanUp() {}
   // eslint-disable-next-line ember/require-super-in-lifecycle-hooks
-  didInsertElement() {},
+  didInsertElement() {}
   // eslint-disable-next-line ember/require-super-in-lifecycle-hooks
-  willDestroyElement() {},
-  keyUp() {},
+  willDestroyElement() {}
+  keyUp() {}
 
   // need to override this to work with the loading slider
   @action
   handleShowUser() {
     return;
-  },
-});
+  }
+}
