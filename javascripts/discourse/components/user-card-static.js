d4c71f953863e0cffb3e0511bfa58121f9df2227
Penar Musaraj <pmusaraj@gmail.com>
2024-10-07 15:03:53 -0400

    Update javascripts/discourse/components/user-card-static.js
    Co-authored-by: Sérgio Saquetim <1108771+megothss@users.noreply.github.com>

--- javascripts/discourse/components/user-card-static.js
+++ javascripts/discourse/components/user-card-static.js
@@ -1,7 +1,7 @@
 import { action } from "@ember/object";
 import UserCardContents from "discourse/components/user-card-contents";
 
-export default class UserCardStaticContents extends UserCardContents.extend() {
+export default class UserCardStaticContents extends UserCardContents {
   layoutName = "components/user-card-contents";
   elementId = null;
   // Overriding functions which cause the user card to show/hide based on mouse/keyboard events:
