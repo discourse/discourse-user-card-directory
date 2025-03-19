b803d64e5175ba94161d84f1d8da79fdfb852826
Jarek Radosz <jradosz@gmail.com>
2025-03-19 12:59:08 +0100

    DEV: Update linting (#52)

--- javascripts/discourse/components/user-card-static.js
+++ javascripts/discourse/components/user-card-static.js
@@ -4,6 +4,7 @@ import UserCardContents from "discourse/components/user-card-contents";
 export default class UserCardStaticContents extends UserCardContents {
   layoutName = "components/user-card-contents";
   elementId = null;
+
   // Overriding functions which cause the user card to show/hide based on mouse/keyboard events:
   cleanUp() {}
 
